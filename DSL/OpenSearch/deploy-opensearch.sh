2b4b4af44f4803a4cd772df861a4874a4908b397
ahmedyasser <26207361+1AhmedYasser@users.noreply.github.com>
2024-06-27 04:08:47 +0300

    Added Companies Employee Information

--- DSL/OpenSearch/deploy-opensearch.sh
+++ DSL/OpenSearch/deploy-opensearch.sh
@@ -9,8 +9,15 @@ if [[ -z $URL || -z $AUTH ]]; then
   exit 1
 fi
 
+# Clear Pipelines
+curl -XDELETE "$URL/_ingest/pipeline/*" -u "$AUTH" --insecure
+
 # Companies
 curl -XDELETE "$URL/companies?ignore_unavailable=true" -u "$AUTH" --insecure
 curl -H "Content-Type: application/x-ndjson" -X PUT "$URL/companies" -ku "$AUTH" --data-binary "@fieldMappings/companies.json"
 curl -L -X POST "$URL/_scripts/company-with-name" -H 'Content-Type: application/json' -H 'Cookie: customJwtCookie=test' --data-binary "@templates/company-with-name.json"
-if $MOCK_ALLOWED; then curl -H "Content-Type: application/x-ndjson" -X PUT "$URL/companies/_bulk" -ku "$AUTH" --data-binary "@mock/companies.json"; fi
+curl -L -X POST "$URL/_scripts/company-employees-with-code" -H 'Content-Type: application/json' -H 'Cookie: customJwtCookie=test' --data-binary "@templates/company-employees-with-code.json"
+curl -L -X POST "$URL/_scripts/companies-employees-with-county" -H 'Content-Type: application/json' -H 'Cookie: customJwtCookie=test' --data-binary "@templates/companies-employees-with-county.json"
+curl -L -X POST "$URL/_scripts/companies-employees" -H 'Content-Type: application/json' -H 'Cookie: customJwtCookie=test' --data-binary "@templates/companies-employees.json"
+curl -L -X PUT "$URL/_ingest/pipeline/convert-employees-to-int" -H 'Content-Type: application/json' -H 'Cookie: customJwtCookie=test' --data-binary "@pipelines/convert-employees-to-int.json"
+if $MOCK_ALLOWED; then curl -H "Content-Type: application/x-ndjson" -X PUT "$URL/companies/_bulk?pipeline=convert-employees-to-int" -ku "$AUTH" --data-binary "@mock/companies.json"; fi
